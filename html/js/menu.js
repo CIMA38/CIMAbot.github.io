a4aa05d3c3ee2e80335a14d43db7e646fa561042
CIMA38 <chatbotcima@gmail.com>
2025-02-24 14:09:48 -0600

    Delete html directory

--- html/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".nav__menu");
-
-    menuToggle.addEventListener("click", () => {
-        menu.classList.toggle("active");
-    });
-});
